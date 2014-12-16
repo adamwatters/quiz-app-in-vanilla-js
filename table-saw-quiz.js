78c70a1a778e56fad02bd2b99139ac3d8853ffef
Adam <adamwatters11@gmail.com>
2014-12-16 13:49:00 -0500

    working

---
+++ table-saw-quiz.js
@@ -0,0 +1,32 @@
+var tableSawQuiz = [
+	{
+		"question": "How high above the wood should the saw blade be set?",
+		"choices": [
+			"As high as it will go",
+			"1/8 to 1/4 inch",
+			"1 to 2 inches",
+			"It shouldn't be set higher than the wood"
+		],
+		"correctAnswer": 1
+	},
+	{
+		"question": "Where is it safest to stand while using the table saw?",
+		"choices": [
+			"About two feet away",
+			"In front of the saw",
+			"To the side of the blade",
+			"On a step stool"
+		],
+		"correctAnswer": 2
+	},
+	{
+		"question": "Reaching over the blade when it is spinning is okay when:",
+		"choices": [
+			"Removing scraps",
+			"Cutting a long piece of wood",
+			"Adjusting the guard",
+			"Never"
+		],
+		"correctAnswer": 3
+	},
+]
